dd007929fcd47580df85a6acf3dad6db4d4ede1a
T-K-KARTHIK-SHANKAR <100600868+T-K-KARTHIK-SHANKAR@users.noreply.github.com>
2022-12-09 10:10:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ init();
 }
 let canvas,ctx,playerNumber,gameActive=false;
 import {io} from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-const socket=io("https://steady-caramel-bda058.netlify.app/");
+const socket=io("https://multiplayersnakegamek.herokuapp.com/");
 socket.on('init',handleInit);
 socket.on('gameState',handleGameSate);
 socket.on('gameOver',handleGameOver);
