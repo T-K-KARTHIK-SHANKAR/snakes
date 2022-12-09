c07460209b837d088f14aae9ff95559ee309ccfd
T-K-KARTHIK-SHANKAR <100600868+T-K-KARTHIK-SHANKAR@users.noreply.github.com>
2022-12-09 12:09:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ init();
 }
 let canvas,ctx,playerNumber,gameActive=false;
 import {io} from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-const socket=io("https://multiplayersnakegamek.herokuapp.com/");
+const socket=io("https://snakeserver.onrender.com");
 socket.on('init',handleInit);
 socket.on('gameState',handleGameSate);
 socket.on('gameOver',handleGameOver);
