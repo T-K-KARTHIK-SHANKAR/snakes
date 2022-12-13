1c6d4f413494cb2ed04ab09860670b2e54aab6ae
T-K-KARTHIK-SHANKAR <100600868+T-K-KARTHIK-SHANKAR@users.noreply.github.com>
2022-12-13 21:09:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ init();
 }
 let canvas,ctx,playerNumber,gameActive=false;
 import {io} from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
-const socket=io("https://websnake.onrender.com");
+const socket=io("https://glitch.com/edit/#!/legend-time-mask?path=server.js%3A140%3A32");
 socket.on('init',handleInit);
 socket.on('gameState',handleGameSate);
 socket.on('gameOver',handleGameOver);
